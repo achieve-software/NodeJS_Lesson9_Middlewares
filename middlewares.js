e994be12204a6b53149d85ccc962153129840eb3
Achieve-software <vtcbcdtyr@gmail.com>
2023-09-29 03:24:32 +0300

    next

--- middlewares.js
+++ middlewares.js
@@ -6,9 +6,19 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
+app.get("/", (req, res, next) => {
+    next()
+  res.send({
+    message: "1.çalıştı",
+  });
+});
+
+
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "2.çalıştı",
+  });
+});
+
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
-
-app.get("/", (req, res) =>{
-
-    
-})
